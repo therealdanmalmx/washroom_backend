4e0c1efc4d42657ce1c6df4d44fbd605c97fb726
therealdanmalmx <danmalmx@gmail.com>
2025-12-26 19:38:43 +0100

    Add get single and post PropertyAdministration

--- API/Controllers/PropertyAdministrationController.cs
+++ API/Controllers/PropertyAdministrationController.cs
@@ -7,7 +7,7 @@ namespace API.Controllers
     [ApiController]
     public class PropertyAdministrationController : ControllerBase
     {
-        private static List<PropertyAdministration> _administrations =
+        private static List<PropertyAdministration> _propertyAdministrations =
         [
             new PropertyAdministration
             {
@@ -15,12 +15,13 @@ namespace API.Controllers
                 Name = "HSB",
                 Logo = "https://www.hsb.se/contentassets/2ba369972d73485598f9c965076774cc/logo_apsis.jpg",
                 CreatedAt = DateTime.Now,
-            },            
+            },
             new PropertyAdministration
             {
                 Id = Guid.NewGuid(),
                 Name = "Riksbyggen",
-                Logo = "https://mnd-assets.mynewsdesk.com/image/upload/c_fill,dpr_auto,f_auto,g_xy_center,q_auto:good,w_1782,x_299,y_207/0wwly1zv6uf8oro3hq4sj6",
+                Logo =
+                    "https://mnd-assets.mynewsdesk.com/image/upload/c_fill,dpr_auto,f_auto,g_xy_center,q_auto:good,w_1782,x_299,y_207/0wwly1zv6uf8oro3hq4sj6",
                 CreatedAt = DateTime.Now,
             },
         ];
@@ -28,8 +29,39 @@ namespace API.Controllers
         [HttpGet]
         public ActionResult<List<PropertyAdministration>> GetAllPropertyAdministrations()
         {
-            return Ok(_administrations);
+            return Ok(_propertyAdministrations);
         }
-        
-    }
+
+        [HttpGet("{id}")]
+        public ActionResult<PropertyAdministration> GetPropertyAdministration(Guid id)
+        {
+            return _propertyAdministrations.SingleOrDefault(p => p.Id == id);
+        }
+
+        [HttpPost]
+        public ActionResult<List<PropertyAdministration>> CreatePropertyAdministration(
+            PropertyAdministration propertyAdministration)
+        {
+            _propertyAdministrations.Add(propertyAdministration);
+            return Ok(_propertyAdministrations);
+        }
+
+        // [HttpPut("{id}")]
+        // public Action<PropertyAdministration> UpdatateProprtyAdministration(Guid id,
+        //     PropertyAdministration propertyAdministration)
+        // {
+        //     PropertyAdministration? paToUpate = _propertyAdministrations.SingleOrDefault(p => p.Id == id);
+        //
+        //     if (!string.IsNullOrEmpty(paToUpate.Name))
+        //     {
+        //         paToUpate.Name = propertyAdministration.Name;
+        //     }
+        //     if (!string.IsNullOrEmpty(paToUpate.Logo))
+        //     {
+        //         paToUpate.Logo = propertyAdministration.Logo;
+        //     }
+        //
+        //     return some;
+        // }
+}
 }
