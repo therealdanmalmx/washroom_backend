7364ed95199b3a6322fdfd57d6ee552044306395
therealdanmalmx <danmalmx@gmail.com>
2025-12-27 21:39:36 +0100

    Update model

--- Core/Models/PropertyManager.cs
+++ Core/Models/PropertyManager.cs
@@ -7,4 +7,5 @@ public class PropertyManager
     public string Phone { get; set; } =  string.Empty;
     public string Email { get; set; } =  string.Empty;
     public int PropertyAdministrationId { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
 }
