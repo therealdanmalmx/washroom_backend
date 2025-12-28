280e1f4337ecf8b464b1e760d67f6c6b33b4a82f
therealdanmalmx <danmalmx@gmail.com>
2025-12-28 21:13:17 +0100

    Split up liness for readability

--- API/Program.cs
+++ API/Program.cs
@@ -10,8 +10,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 builder.Services.AddScoped<IPropertyAdministrationRepository, PropertyAdministrationRepository>();
 builder.Services.AddScoped<IPropertyAdministrationService, PropertyAdministrationService>();
+
 builder.Services.AddScoped<IPropertyManagerRepository, PropertyManagerRepository>();
 builder.Services.AddScoped<IPropertyManagerService, PropertyManagerService>();
 
