7dce474bedb3f2db599e3a4145d6c88117d9ded6
therealdanmalmx <danmalmx@gmail.com>
2025-12-29 21:22:13 +0100

    Ass Create, Single, Update and Delete to service

--- API/Services/Association/AssociationService.cs
+++ API/Services/Association/AssociationService.cs
@@ -22,4 +22,50 @@ public class AssociationService : IAssociationService
         }
         return result.Adapt<List<AssociationGetAllDto>>();
     }
+
+    public List<AssociationCreateDto> CreateAssociation(AssociationCreateDto newAssociation)
+    {
+        var newAssociationEntry = newAssociation.Adapt<Core.Models.Association>();
+        var result = _associationRepository.CreateAssociation(newAssociationEntry);
+
+        if (result == null)
+        {
+            return null;
+        }
+        return  result.Adapt<List<AssociationCreateDto>>(); 
+    }
+
+    public AssociationCreateDto? GetAssociationById(int id)
+    {
+       var singleAssociation =  _associationRepository.GetAssociationById(id);
+       if (singleAssociation == null)
+       {
+           return null;
+       }
+       return singleAssociation.Adapt<AssociationCreateDto>();
+    }
+
+    public List<AssociationUpdateDto> UpdateAssociation(int id, AssociationUpdateDto association)
+    {
+        var associationToUpdate = association.Adapt<Core.Models.Association>();
+        var result = _associationRepository.UpdateAssociation(id, associationToUpdate);
+        if (result == null)
+        {
+            return null;
+        }
+        return result.Adapt<List<AssociationUpdateDto>>();
+        
+    }
+
+    public List<AssociationGetAllDto>? DeleteAssociation(int id)
+    {
+        var result = _associationRepository.DeleteAssociation(id);
+
+        if (result == null)
+        {
+            return null;
+        }
+        return result.Adapt<List<AssociationGetAllDto>>();
+        
+    }
 }
