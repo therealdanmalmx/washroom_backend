e63233293e32a645d64dce5d9dd10cbe77bb7a47
therealdanmalmx <danmalmx@gmail.com>
2025-12-29 21:21:09 +0100

    Ass Create, Single, Update and Delete repository interface

--- API/Repositories/Association/IAssociationRepository.cs
+++ API/Repositories/Association/IAssociationRepository.cs
@@ -5,4 +5,8 @@ namespace API.Repositories;
 public interface IAssociationRepository
 {
     List<Association> GetAllAssociations();
+    List<Association> CreateAssociation(Association association);
+    Association? GetAssociationById(int id);
+    List<Association> UpdateAssociation(int id, Association association);
+    List<Association>? DeleteAssociation(int id);
 }
