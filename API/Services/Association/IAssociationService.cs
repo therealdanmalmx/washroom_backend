8fbdb356c8513b46ea829bcec945c03401c9e62f
therealdanmalmx <danmalmx@gmail.com>
2025-12-29 21:21:59 +0100

    Ass Create, Single, Update and Delete to service interface

--- API/Services/Association/IAssociationService.cs
+++ API/Services/Association/IAssociationService.cs
@@ -5,4 +5,11 @@ namespace API.Services.Association;
 public interface IAssociationService
 {
     List<AssociationGetAllDto> GetAllAssociations();
+    List<AssociationCreateDto> CreateAssociation(AssociationCreateDto newAssociation);
+    AssociationCreateDto? GetAssociationById(int id);
+    List<AssociationUpdateDto> UpdateAssociation(int id, AssociationUpdateDto association);
+    List<AssociationGetAllDto>? DeleteAssociation(int id);
+
+
+
 }
