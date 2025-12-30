3fb9435dd03de1def7b71104230e3a88c9cd85a0
therealdanmalmx <danmalmx@gmail.com>
2025-12-30 20:28:45 +0100

    Add Apartment model

---
+++ Core/Models/Apartment.cs
@@ -0,0 +1,9 @@
+namespace Core.Models;
+
+public class Apartment
+{
+    public int Id { get; set; }
+    public string Number { get; set; } = string.Empty;
+    public int PropertyId { get; set; }
+    public DateTime CreatedAt { get; set; } =  DateTime.Now;
+}
