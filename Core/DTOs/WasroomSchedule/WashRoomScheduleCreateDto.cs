c05bea6d192b9f4c1ceee42adf1b3163a8415992
therealdanmalmx <danmalmx@gmail.com>
2025-12-31 16:01:48 +0100

    Add WashRoomSchedule DTO's

---
+++ Core/DTOs/WasroomSchedule/WashRoomScheduleCreateDto.cs
@@ -0,0 +1,8 @@
+namespace Core.DTOs.WasroomSchedule;
+
+public class WashRoomScheduleCreateDto
+{
+    public int Id { get; set; }
+    public int ScheduleId { get; set; }
+    public int  WashRoomId { get; set; }
+}
---
+++ Core/DTOs/WasroomSchedule/WashRoomScheduleUpdateDto.cs
@@ -0,0 +1,8 @@
+namespace Core.DTOs.WasroomSchedule;
+
+public class WashRoomScheduleUpdateDto
+{
+    public int Id { get; set; }
+    public int ScheduleId { get; set; }
+    public int  WashRoomId { get; set; }
+}
