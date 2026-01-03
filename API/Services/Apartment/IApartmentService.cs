24a2c27c939b8db278d372f894e92e69d508120a
therealdanmalmx <danmalmx@gmail.com>
2026-01-03 21:23:13 +0100

    Add async/await to GetAll, GetById, Update and delete in service interface

--- API/Services/Apartment/IApartmentService.cs
+++ API/Services/Apartment/IApartmentService.cs
@@ -4,9 +4,9 @@ namespace API.Services.Apartment;
 
 public interface IApartmentService
 {
-    List<ApartmentGetAllDto> GetAllApartments();
+    Task<List<ApartmentGetAllDto>> GetAllApartments();
     Task<List<ApartmentGetAllDto>> CreateApartment(ApartmentCreateDto newApartment);
-    ApartmentGetAllDto? GetApartmentById(int id);
-    List<ApartmentGetAllDto>? UpdateApartment(int id, ApartmentUpdateDto updatedApartment);
-    List<ApartmentGetAllDto>? DeleteApartment(int id);
+    Task<ApartmentGetAllDto?> GetApartmentById(int id);
+    Task<List<ApartmentGetAllDto>>? UpdateApartment(int id, ApartmentUpdateDto updatedApartment);
+    Task<List<ApartmentGetAllDto>>? DeleteApartment(int id);
 }
