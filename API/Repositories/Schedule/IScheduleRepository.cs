7e1be32a9830bee8f7ec5359c25c9ab3b94a3ed0
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 19:12:38 +0100

    Add async/await for Schedule CRUD - repository interface

--- API/Repositories/Schedule/IScheduleRepository.cs
+++ API/Repositories/Schedule/IScheduleRepository.cs
@@ -4,9 +4,9 @@ namespace API.Repositories.ScheduleRepository;
 
 public interface IScheduleRepository
 {
-    List<Schedule> GetAllSchedules();
-    List<Schedule> CreateSchedules(Schedule newSchedule);
-    Schedule? GetScheduleById(int id);
-    List<Schedule>? UpdateSchedule(int id,Schedule updateSchedule);
-    List<Schedule>? DeleteSchedule(int id);
+    Task<List<Schedule>> GetAllSchedules();
+    Task<List<Schedule>> CreateSchedules(Schedule newSchedule);
+    Task<Schedule>? GetScheduleById(int id);
+    Task<List<Schedule>>? UpdateSchedule(int id,Schedule updateSchedule);
+    Task<List<Schedule>>? DeleteSchedule(int id);
 }
