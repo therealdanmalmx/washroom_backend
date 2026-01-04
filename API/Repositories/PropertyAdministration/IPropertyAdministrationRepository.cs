d3cb6dc4d630571ef42f34ee522460821f5c872d
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 18:28:39 +0100

    Add async/await to PropertyAdministration CRUD - repository interface

--- API/Repositories/PropertyAdministration/IPropertyAdministrationRepository.cs
+++ API/Repositories/PropertyAdministration/IPropertyAdministrationRepository.cs
@@ -4,9 +4,9 @@ namespace API.Repositories;
 
 public interface IPropertyAdministrationRepository
 {
-    List<PropertyAdministration> GetAllPropertyAdministrations();
-    PropertyAdministration GetPropertyAdministration(int id);
-    List<PropertyAdministration> CreatePropertyAdministration(PropertyAdministration newPropertyAdministration);
-    List<PropertyAdministration>? UpdatePropertyAdministration(int id, PropertyAdministration updatePropertyAdministration);
-    List<PropertyAdministration> DeletePropertyAdministration(int id);
+    Task<List<PropertyAdministration>> GetAllPropertyAdministrations();
+    Task<List<PropertyAdministration>> CreatePropertyAdministration(PropertyAdministration newPropertyAdministration);
+    Task<PropertyAdministration>? GetPropertyAdministration(int id);
+    Task<List<PropertyAdministration>>? UpdatePropertyAdministration(int id, PropertyAdministration updatePropertyAdministration);
+    Task<List<PropertyAdministration>>? DeletePropertyAdministration(int id);
 }
