89add1a28646016420391369e4921e43c3abf35e
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 18:01:33 +0100

    Update Property Update DTO

--- Core/DTOs/Property/PropertyUpdateDto.cs
+++ Core/DTOs/Property/PropertyUpdateDto.cs
@@ -2,7 +2,6 @@ namespace Core.DTOs.Property;
 
 public class PropertyUpdateDto
 {
-    public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Street { get; set; } = string.Empty;
     public string Number { get; set; } = string.Empty;
