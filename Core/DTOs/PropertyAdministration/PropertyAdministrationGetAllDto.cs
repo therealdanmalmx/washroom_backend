988e16f67c476bdbf27f1fa0e8f82665489bd916
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 23:45:48 +0100

    Chnage Id type to Guid

--- Core/DTOs/PropertyAdministration/PropertyAdministrationGetAllDto.cs
+++ Core/DTOs/PropertyAdministration/PropertyAdministrationGetAllDto.cs
@@ -2,7 +2,7 @@ namespace Core.DTOs.PropertyAdministration;
 
 public class PropertyAdministrationGetAllDto
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     public required string Name { get; set; }
     public string Logo { get; set; } = string.Empty;
 }
