2a83113a301a5dda98580e2469e5e16b1f22a494
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 19:54:34 +0100

    Add async/await to Tentat CRUD - repository interface

--- API/Repositories/Tenant/ITenantRepository.cs
+++ API/Repositories/Tenant/ITenantRepository.cs
@@ -2,9 +2,9 @@ using Core.Models;
 namespace API.Repositories;
 public interface ITenantRepository
 {
-    List<Tenant> GetAllTenant();
-    List<Tenant> CreateTenant(Tenant tenant);
-    Tenant? GetTenantById(int id);
-    List<Tenant>? UpdateTenant(int id, Tenant newTenant);
-    List<Tenant>? DeleteTenant(int id);
+    Task<List<Tenant>> GetAllTenant();
+    Task<List<Tenant>> CreateTenant(Tenant tenant);
+    Task<Tenant>? GetTenantById(int id);
+    Task<List<Tenant>>? UpdateTenant(int id, Tenant newTenant);
+    Task<List<Tenant>>? DeleteTenant(int id);
 }
