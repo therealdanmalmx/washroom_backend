562877dc4546e3e97ff8cff79323034367f31e1d
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 19:55:44 +0100

    Change to UtcNow

--- Core/Models/Tenant.cs
+++ Core/Models/Tenant.cs
@@ -7,5 +7,5 @@ public class Tenant
     public string Phone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public int ApartmentId { get; set; }
-    public DateTime CreatedAt { get; set; } =  DateTime.Now;
+    public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
 }
