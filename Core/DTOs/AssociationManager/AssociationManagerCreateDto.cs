bb403d8de3ac4dc579187ccf2cc3e2db6030d0ca
therealdanmalmx <danmalmx@gmail.com>
2026-01-04 23:52:23 +0100

    Change Id type to Guid

--- Core/DTOs/AssociationManager/AssociationManagerCreateDto.cs
+++ Core/DTOs/AssociationManager/AssociationManagerCreateDto.cs
@@ -5,5 +5,5 @@ public class AssociationManagerCreateDto
     public string Name { get; set; } = string.Empty;
     public string Phone { get; set; } =  string.Empty;
     public string Email { get; set; } =  string.Empty;
-    public int AssociationId { get; set; }
+    public Guid AssociationId { get; set; }
 }
--- Core/DTOs/AssociationManager/AssociationManagerGetAllDto.cs
+++ Core/DTOs/AssociationManager/AssociationManagerGetAllDto.cs
@@ -6,5 +6,5 @@ public class AssociationManagerGetAllDto
     public string Name { get; set; } = string.Empty;
     public string Phone { get; set; } =  string.Empty;
     public string Email { get; set; } =  string.Empty;
-    public int AssociationId { get; set; }
+    public Guid AssociationId { get; set; }
 }
