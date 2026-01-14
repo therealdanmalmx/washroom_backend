50bea3485589616a575b7210da5d89cf2b8742d3
therealdanmalmx <danmalmx@gmail.com>
2026-01-14 19:30:28 +0100

    Add one to many relationships

--- Core/Models/Apartment.cs
+++ Core/Models/Apartment.cs
@@ -5,5 +5,7 @@ public class Apartment
     public Guid Id { get; set; } =  Guid.CreateVersion7();
     public string Number { get; set; } = string.Empty;
     public Guid PropertyId { get; set; }
+    public Property Property { get; set; } = new();
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public List<Tenant> Tenants { get; set; } = new();
 }
