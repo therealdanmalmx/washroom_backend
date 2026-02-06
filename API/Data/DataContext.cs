b46b850dea9f5d5ce3dbb52f34054b07ac9f12fb
therealdanmalmx <danmalmx@gmail.com>
2026-02-06 07:53:08 +0100

    Add IdentityDbContext

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,9 +1,10 @@
 using Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
 
-public class DataContext : DbContext
+public class DataContext : IdentityDbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
