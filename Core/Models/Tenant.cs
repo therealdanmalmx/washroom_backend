8931b1a52fca5dec23616e813f5cc4782af0f3f9
therealdanmalmx <danmalmx@gmail.com>
2026-02-17 12:49:43 +0100

    Use IdentityUser on Tenant

--- Core/Models/Tenant.cs
+++ Core/Models/Tenant.cs
@@ -1,11 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
 namespace Core.Models;
 
-public class Tenant
+public class Tenant : IdentityUser
 {
-    public Guid Id { get; set; } = Guid.CreateVersion7();
-    public string Name { get; set; } = string.Empty;
+    [StringLength((12))]
     public string Phone { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
     public Guid ApartmentId { get; set; }
     public Apartment Apartment { get; set; } = new();
     public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
