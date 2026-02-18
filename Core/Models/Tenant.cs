0e2164abe93e51fcf0eb9d14f1faaabad2d5a7a8
therealdanmalmx <danmalmx@gmail.com>
2026-02-18 07:59:10 +0100

    Remove phone (use Identity). Set initi only on remaining fields.

--- Core/Models/Tenant.cs
+++ Core/Models/Tenant.cs
@@ -5,10 +5,8 @@ namespace Core.Models;
 
 public class Tenant : IdentityUser
 {
-    [StringLength((12))]
-    public string Phone { get; set; } = string.Empty;
-    public Guid ApartmentId { get; set; }
-    public Apartment Apartment { get; set; } = new();
-    public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
+    public Guid ApartmentId { get; init; }
+    public Apartment Apartment { get; init; } = new();
+    public DateTime CreatedAt { get; init; } =  DateTime.UtcNow;
     public List<TenantWashroomBooking> TenantWashroomBookings { get; set; } = new();
 }
