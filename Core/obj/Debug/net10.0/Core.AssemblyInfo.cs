ec2cf5ba2206f84d250aa3c15a702afe28ac8c29
therealdanmalmx <danmalmx@gmail.com>
2026-02-21 19:14:25 +0100

    Upadate files

--- Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net10.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26e985f2ef229b91dfd5c18681099b165cc246ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e2164abe93e51fcf0eb9d14f1faaabad2d5a7a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
