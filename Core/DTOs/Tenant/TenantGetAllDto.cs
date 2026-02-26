a4e763c81bec5d5962960fb0d523bf359b73fb25
therealdanmalmx <danmalmx@gmail.com>
2026-02-26 15:28:22 +0100

    Inherit tenat DTO's from Identity

--- Core/DTOs/Tenant/TenantGetAllDto.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Core.DTOs.Tenant;
-
-public class TenantGetAllDto : IdentityUser
-{
-    public Guid ApartmentId { get; set; }
-}
--- Core/DTOs/Tenant/TenantUpdateDto.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace Core.DTOs.Tenant;
-
-public class TenantUpdateDto : IdentityUser
-{
-    public Guid ApartmentId { get; set; }
-
-}
