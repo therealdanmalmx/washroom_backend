74c76be0ecd686f8f0e874eab9e7cd5b92bb0bbb
therealdanmalmx <danmalmx@gmail.com>
2026-02-26 15:28:36 +0100

    Add contructor to Tenant

--- Core/Models/Tenant.cs
+++ Core/Models/Tenant.cs
@@ -6,9 +6,20 @@ namespace Core.Models;
 public class Tenant : IdentityUser
 {
     public string ApartmentId { get; init; }
-    public Apartment Apartment { get; init; } = new();
+    public Apartment Apartment { get; init; }
     public string PropertyId { get; init; }
-    public Property Property { get; set; } = new();
-    public DateTime CreatedAt { get; init; } =  DateTime.UtcNow;
+    public Property Property { get; set; }
+    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
     public List<TenantWashroomBooking> TenantWashroomBookings { get; set; } = new();
+
+    public Tenant() { }
+    public Tenant(string userName, string email, string phoneNumber, string apartmentId,
+        string propertyId)
+    {
+        UserName = userName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        ApartmentId = apartmentId;
+        PropertyId = propertyId;
+    }
 }
